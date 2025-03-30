e47b928e3bacf6f5b39d63e527278949db04b2e8
Tommertom <Tom.Gruintjes@gmail.com>
2025-03-30 19:01:20 +0200

    Cleaned up

--- components/PageEmpty.svelte
+++ components/PageEmpty.svelte
@@ -1,6 +1,6 @@
 <script>
 	console.warn(
-		`Please replace import { IonPage } from 'ionic-svelte' with import IonPage from 'ionic-svelte/components/IonPage.svelte'`
+		`Please replace import { IonPage } from 'ionic-svelte' with import IonPage from '@ionic-svelte/components/IonPage.svelte'`
 	);
 </script>
 
